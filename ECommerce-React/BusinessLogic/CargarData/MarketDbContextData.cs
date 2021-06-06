43dfe6532e393d7ee44752b2862697d81a453eec
Joaquin Salas <joaquinsalas-1993@hotmail.com>
2021-06-05 20:32:07 -0600

    Update 5-6-2021 8:32 pm

---
+++ ECommerce-React/BusinessLogic/CargarData/MarketDbContextData.cs
@@ -0,0 +1,65 @@
+﻿using BusinessLogic.Data;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.CargarData
+{
+    public class MarketDbContextData
+    {
+        public static async Task CargarDataAsync(MarketDbContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.Marca.Any()) /*indica si no tiene algun valor al menos en la tabla*/
+                {
+                    var marcaData = File.ReadAllText("../BusinessLogic/CargarData/marca.json");
+                    var marcas = JsonSerializer.Deserialize<List<Marca>>(marcaData);
+
+                    foreach (var marca in marcas)
+                    {
+                        await context.Marca.AddAsync(marca);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Categoria.Any()) /*indica si no tiene algun valor al menos en la tabla*/
+                {
+                    var categoriaData = File.ReadAllText("../BusinessLogic/CargarData/categoria.json");
+                    var categorias = JsonSerializer.Deserialize<List<Categoria>>(categoriaData);
+
+                    foreach (var categoria in categorias)
+                    {
+                        await context.Categoria.AddAsync(categoria);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Producto.Any()) /*indica si no tiene algun valor al menos en la tabla*/
+                {
+                    var productoData = File.ReadAllText("../BusinessLogic/CargarData/producto.json");
+                    var productos = JsonSerializer.Deserialize<List<Producto>>(productoData);
+
+                    foreach (var producto in productos)
+                    {
+                        await context.Producto.AddAsync(producto);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<MarketDbContextData>();
+                logger.LogError(e.Message);
+            }
+        }
+    }
+}
--- ECommerce-React/WebApi/Program.cs
+++ ECommerce-React/WebApi/Program.cs
@@ -1,19 +1,39 @@
+using BusinessLogic.CargarData;
+using BusinessLogic.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                /*Realiza el proceso de migración*/
+                var context = services.GetRequiredService<MarketDbContext>();
+                await context.Database.MigrateAsync();
+                await MarketDbContextData.CargarDataAsync(context, loggerFactory);
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(e, "Errores en el proceso de migración");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
